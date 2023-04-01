868f59ff05d54a05e6c035358f9c06e2443cc052
luan004 <luan.gabriel.ifpr@gmail.com>
2023-04-01 14:45:36 -0300

    01-04

--- panel/js/data/customizationFunctions.js
+++ panel/js/data/customizationFunctions.js
@@ -1,10 +1,82 @@
-function uploadImage(slideNum, image) {
-    return firebase.storage().ref().child(`customizations/slideshow/slide${slideNum}.jpg`).put(image)
+function changeSwitchValue(swit) {
+    firebase.database().ref('custom/'+swit+'/enable').once('value').then((snap) => {
+        if (snap.val() == false) {
+            firebase.database().ref('custom/'+swit).update({'enable':true});
+        } else {
+            firebase.database().ref('custom/'+swit).update({'enable':false});
+        }
+    })
+}
+
+function loadSwitch(switchid) {
+    firebase.database().ref('custom/'+switchid+'/enable').once('value').then((snap) => {
+        if (snap.val() == true) {
+            document.getElementById(switchid).checked = true;
+        }
+    })
+}
+
+function loadCarouselProduct(num) {
+    Promise.resolve(getCarouselProduct(num)).then((value) => {
+        firebase.database().ref('products/').child(value).once('value').then((snap) => {
+            document.getElementById('product'+num+'img').src = snap.val().image;
+            document.getElementById('product'+num+'name').innerHTML = snap.val().name;
+            document.getElementById('product'+num+'desc').innerHTML = snap.val().description;
+            document.getElementById('product'+num+'price').innerHTML = 'R$'+snap.val().price.replace('.', ',');
+            document.getElementById('product'+num+'id').value = value;
+        })
+    })
+}
+
+
+function getCarouselProduct(num) {
+    return firebase.database().ref("custom/carousel/product" + num).once('value')
     .then((snapshot) => {
-        return snapshot.ref.getDownloadURL();
+        return snapshot.val();
+    })
+}
+
+function uploadSlideFile(file, num) {
+    return firebase.storage().ref().child(`custom/slideshow/slide${num}.jpg`).put(file)
+    .then((snapshot) => {
+        return true;
     })
     .catch((error) => {
         console.error('Error uploading file:', error);
+        return false;
+    });
+}
+
+function getSlideSrc(num) {
+    return firebase.storage().ref().child(`custom/slideshow/slide${num}.jpg`).getDownloadURL()
+    .then(url => {
+        return url;
+    })
+    .catch(error => {
+        console.error('Error getting download URL:', error);
         return null;
     });
+}
+
+function changeCarouselTitle(title) {
+    if(title == '') {title = 'Destaques'}
+    const value = {
+        "title": title,
+    };
+    firebase.database().ref('custom/carousel/').update(value);
+}
+
+function loadCarouselTitle() {
+    firebase.database().ref('custom/carousel/title').once('value')
+    .then((snapshot) => {
+        document.getElementById("inputTitle").value = snapshot.val();
+    })
+}
+
+function changeCarouselProduct(id, num) {
+    var product = 'product'+num;
+    const value = {
+        [product]: id
+    };
+    firebase.database().ref('custom/carousel/').update(value);
 }
--- panel/js/pages/customization.js
+++ panel/js/pages/customization.js
@@ -96,43 +96,10 @@ document.getElementById('product4id').addEventListener('blur', function() {
     })
 })
 
-/* CHANGE SWITCH VALUES */
-function changeSwitchValue(swit) {
-    firebase.database().ref('custom/'+swit+'/enable').once('value').then((snap) => {
-        if (snap.val() == false) {
-            firebase.database().ref('custom/'+swit).update({'enable':true});
-        } else {
-            firebase.database().ref('custom/'+swit).update({'enable':false});
-        }
-    })
-}
-
-/* LOAD SWITCHS VALUES */
-function loadSwitch(switchid) {
-    firebase.database().ref('custom/'+switchid+'/enable').once('value').then((snap) => {
-        if (snap.val() == true) {
-            document.getElementById(switchid).checked = true;
-        }
-    })
-}
-
 /* LOAD SLIDES FROM DATA */
 function loadSlide(num) {
     Promise.resolve(getSlideSrc(num)).then((val) => {
         document.getElementById("slide"+num).src = val;
-    })
-}
-
-/* LOAD PRODUCTS FROM DATA */
-function loadCarouselProduct(num) {
-    Promise.resolve(getCarouselProduct(num)).then((value) => {
-        firebase.database().ref('products/').child(value).once('value').then((snap) => {
-            document.getElementById('product'+num+'img').src = snap.val().image;
-            document.getElementById('product'+num+'name').innerHTML = snap.val().name;
-            document.getElementById('product'+num+'desc').innerHTML = snap.val().description;
-            document.getElementById('product'+num+'price').innerHTML = 'R$'+snap.val().price.replace('.', ',');
-            document.getElementById('product'+num+'id').value = value;
-        })
     })
 }
 
@@ -149,54 +116,12 @@ async function selectJpgFile() {
     return file;
 }
 
-function getCarouselProduct(num) {
-    return firebase.database().ref("custom/carousel/product" + num).once('value')
-    .then((snapshot) => {
-        return snapshot.val();
-    })
-}
-
-function uploadSlideFile(file, num) {
-    return firebase.storage().ref().child(`custom/slideshow/slide${num}.jpg`).put(file)
-    .then((snapshot) => {
-        return true;
-    })
-    .catch((error) => {
-        console.error('Error uploading file:', error);
-        return false;
-    });
-}
-
-function getSlideSrc(num) {
-    return firebase.storage().ref().child(`custom/slideshow/slide${num}.jpg`).getDownloadURL()
-    .then(url => {
-        return url;
-    })
-    .catch(error => {
-        console.error('Error getting download URL:', error);
-        return null;
-    });
-}
-
-function changeCarouselTitle(title) {
-    if(title == '') {title = 'Destaques'}
-    const value = {
-        "title": title,
-    };
-    firebase.database().ref('custom/carousel/').update(value);
-}
-
-function loadCarouselTitle() {
-    firebase.database().ref('custom/carousel/title').once('value')
-    .then((snapshot) => {
-        document.getElementById("inputTitle").value = snapshot.val();
-    })
-}
-
-function changeCarouselProduct(id, num) {
-    var product = 'product'+num;
-    const value = {
-        [product]: id
-    };
-    firebase.database().ref('custom/carousel/').update(value);
+function downloadFile(url) {
+    const link = document.createElement('a');
+    link.href = url;
+    link.target = '_blank';
+    link.download = '';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
