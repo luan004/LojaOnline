e11b4b94f47c72bd4324a88e7e92519bb54e2879
Luan Gabriel <luan.gabriel.ifpr@gmail.com>
2023-05-06 14:22:02 -0300

    highlights search

--- web/js/layout/highlights.js
+++ web/js/layout/highlights.js
@@ -2,66 +2,28 @@ import {readProduct} from '../data/productCRUD.js';
 import {getData} from '../data/customFunctions.js';
 
 $(document).ready(function() {
-    getData('carousel/enable').then((val) => {
-      if (val == true) {
-        $("#highlights").load("../web/resources/layout/highlights.html", function() {
-          /* TITLE */
-          getData('carousel/title').then((val) => {
-            document.getElementById('titleHightlights').innerHTML = val;
-          })
+  getData('carousel/enable').then((val) => {
+    if (val == true) {
+      $("#highlights").load("../web/resources/layout/highlights.html", function() {
+        /* TITLE */
+        getData('carousel/title').then((val) => {
+          document.getElementById('titleHightlights').innerHTML = val;
+        })
 
-          var i = 1;
-          while(i <= 4) {
-            console.log(i);
-            getData('carousel/product1').then((val) => {
+        /* LOAD PRODUCTS */
+        for (let i = 1; i <= 4; i++) {
+          (function(index) {
+            getData('carousel/product' + index).then((val) => {
               readProduct(val).then((product) => {
-                document.getElementById('product1img').src = product.image;
-                document.getElementById('product1name').innerHTML = product.name;
-                document.getElementById('product1name').href = './product?key=' + val;
-                document.getElementById('product1price').innerHTML = 'R$' + product.price;
+                document.getElementById('product' + index + 'img').src = product.image;
+                document.getElementById('product' + index + 'name').innerHTML = product.name;
+                document.getElementById('product' + index + 'name').href = './product?key=' + val;
+                document.getElementById('product' + index + 'price').innerHTML = 'R$' + product.price;
               })
             })
-
-            i++;
-          }
-
-  
-/*           getData('carousel/product1').then((val) => {
-            readProduct(val).then((product) => {
-              document.getElementById('product1img').src = product.image;
-              document.getElementById('product1name').innerHTML = product.name;
-              document.getElementById('product1name').href = './product?key=' + val;
-              document.getElementById('product1price').innerHTML = 'R$' + product.price.replace('.', ',');
-            })
-          })
-  
-          getData('carousel/product2').then((val) => {
-            readProduct(val).then((product) => {
-              document.getElementById('product2img').src = product.image;
-              document.getElementById('product2name').innerHTML = product.name;
-              document.getElementById('product2name').href = './product?key=' + val;
-              document.getElementById('product2price').innerHTML = 'R$' + product.price.replace('.', ',');
-            })
-          })
-
-          getData('carousel/product3').then((val) => {
-            readProduct(val).then((product) => {
-              document.getElementById('product3img').src = product.image;
-              document.getElementById('product3name').innerHTML = product.name;
-              document.getElementById('product3name').href = './product?key=' + val;
-              document.getElementById('product3price').innerHTML = 'R$' + product.price;
-            })
-          })
-  
-          getData('carousel/product4').then((val) => {
-            readProduct(val).then((product) => {
-              document.getElementById('product4img').src = product.image;
-              document.getElementById('product4name').innerHTML = product.name;
-              document.getElementById('product4name').href = './product?key=' + val;
-              document.getElementById('product4price').innerHTML = 'R$' + product.price.replace('.', ',');
-            })
-          })   */
-        })
-      }
-    })
+          })(i);
+        }
+      })
+    }
+  })
 })
--- web/js/pages/search.js
+++ web/js/pages/search.js
@@ -65,7 +65,7 @@ function createListItem(key, product) {
                 <span class="badge text-bg-secondary">`+ product.category+`</span>
 
                 <div class="position-absolute bottom-0 mb-2 fill-available me-2 pt-2 bg-white">
-                    <h4 class="me-auto mb-2 text-truncate">R$` + product.price.replace('.', ',') + `</h4>
+                    <h4 class="me-auto mb-2 text-truncate">R$` + product.price + `</h4>
                 </div>
             </div>
         </div>
@@ -107,56 +107,3 @@ function removeStopWords(string) {
     // Chamar funcao increaseSearchStat para cada
     wordsWithoutStopWords.forEach(word => increaseSearchStat(word));
 }
-
-$(document).ready(function() {
-  getData('carousel/enable').then((val) => {
-    if (val == true) {
-      $("#highlights").load("../web/resources/layout/highlights.html", function() {
-        /* TITLE */
-        getData('carousel/title').then((val) => {
-          document.getElementById('titleHightlights').innerHTML = val;
-        })
-
-        /* 1 */
-        getData('carousel/product1').then((val) => {
-          readProduct(val).then((product) => {
-            document.getElementById('product1img').src = product.image;
-            document.getElementById('product1name').innerHTML = product.name;
-            document.getElementById('product1name').href = './product?key=' + val;
-            document.getElementById('product1price').innerHTML = 'R$' + product.price.replace('.', ',');
-          })
-        })
-
-        /* 2 */
-        getData('carousel/product2').then((val) => {
-          readProduct(val).then((product) => {
-            document.getElementById('product2img').src = product.image;
-            document.getElementById('product2name').innerHTML = product.name;
-            document.getElementById('product2name').href = './product?key=' + val;
-            document.getElementById('product2price').innerHTML = 'R$' + product.price.replace('.', ',');
-          })
-        })
-
-        /* 3 */
-        getData('carousel/product3').then((val) => {
-          readProduct(val).then((product) => {
-            document.getElementById('product3img').src = product.image;
-            document.getElementById('product3name').innerHTML = product.name;
-            document.getElementById('product3name').href = './product?key=' + val;
-            document.getElementById('product3price').innerHTML = 'R$' + product.price.replace('.', ',');
-          })
-        })
-
-        /* 4 */
-        getData('carousel/product4').then((val) => {
-          readProduct(val).then((product) => {
-            document.getElementById('product4img').src = product.image;
-            document.getElementById('product4name').innerHTML = product.name;
-            document.getElementById('product4name').href = './product?key=' + val;
-            document.getElementById('product4price').innerHTML = 'R$' + product.price.replace('.', ',');
-          })
-        })  
-      })
-    }
-  })
-})
