121e08c9e6e4da9d76db158fb44969199dfff17e
Luan Gabriel <luan.gabriel.ifpr@gmail.com>
2023-05-09 11:00:33 -0300

    Create js.js

---
+++ js.js
@@ -0,0 +1,18 @@
+/* String */
+var nome = 'valor';
+
+/* Number */
+var idade = 10;
+
+/* Boolean */
+var ligado = true;
+
+/* Array */
+var meuArray = ['banana', 20, 'pera', true];
+
+
+/* Constantes */
+const nome = 'valor';
+const idade = 10;
+const ligado = true;
+const meuArray = ['banana', 20, 'pera', true];
